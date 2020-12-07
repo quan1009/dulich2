df0b95b3a18883a159ac848693e29b2ca171e573
quan1009 <75003204+quan1009@users.noreply.github.com>
2020-12-07 19:07:01 +0700

    Hello

--- app/src/main/java/com/example/dulich/MainActivity.kt
+++ app/src/main/java/com/example/dulich/MainActivity.kt
@@ -2,10 +2,15 @@ package com.example.dulich
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+public class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
     }
 }
